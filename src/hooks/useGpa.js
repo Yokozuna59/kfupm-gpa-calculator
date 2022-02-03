73a177019beaabd66eb4061f56b6e2efc5b9d161
mrbasel <mrbasel555@gmail.com>
2022-02-03 11:46:57 +0300

    Bring current gpa option back

--- src/hooks/useGpa.js
+++ src/hooks/useGpa.js
@@ -1,16 +1,16 @@
 import { calculateGPA, calculateTotalHours } from "../utils";
 
 export function useGpa(courses, currentGpa = 0, currentHours = 0) {
-  let gpa = calculateGPA(courses);
-  let totalHours = calculateTotalHours(courses);
+  let gpa = parseFloat(calculateGPA(courses));
+  let totalHours = parseInt(calculateTotalHours(courses), 10);
 
   if (currentGpa && currentHours) {
     gpa = (
-      (gpa * totalHours + parseFloat(gpa) * parseInt(currentHours)) /
-      (totalHours + parseInt(currentHours))
+      (gpa * totalHours + parseFloat(currentGpa) * parseInt(currentHours, 10)) /
+      (totalHours + parseInt(currentHours, 10))
     ).toFixed(3);
 
-    totalHours += parseInt(currentHours);
+    totalHours += parseInt(currentHours, 10);
   }
 
   return {
